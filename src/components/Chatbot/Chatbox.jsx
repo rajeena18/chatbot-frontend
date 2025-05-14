69028e24e16e587307df544cc506c5e6d179431b
Rahul Jeena <rajeena@alletec.com>
2025-05-14 15:17:02 +0530

    Initial commit

---
+++ src/components/Chatbot/Chatbox.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Chatbox = () => {
+  return (
+    <div style={{ width: '100%', height: '500px' }}>
+      <iframe src="https://copilotstudio.microsoft.com/environments/Default-67c5a58a-7424-4d4d-b6c2-ddc89830cf74/bots/tri_alletecInfoBot/webchat?__version__=2" frameBorder="0"
+        style={{ width: '100%', height: '600px', border: '1px solid #ccc', borderRadius: '8px' }}></iframe>
+    </div>
+  );
+};
+
+export default Chatbox;
